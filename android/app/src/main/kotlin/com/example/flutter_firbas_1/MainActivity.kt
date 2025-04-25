832d026231549a3b0cd58a9846c4d91f270a325c
mahmoud7-flutter <mahmoudghabsha771@gmail.com>
2025-04-25 15:55:24 -0700

    Initial GitHub Pages commit

--- android/app/src/main/kotlin/com/example/flutter_firbas_1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_firbas_1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/models/models.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class models {
-  final String id;
-  final String title;
-  final String imageUrl;
- const models( { required this.id ,required this.imageUrl, required this.title});
-}
